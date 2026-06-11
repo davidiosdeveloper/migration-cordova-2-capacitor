c997fd37f78ab46b4e5b9892325d60126d1f9201
iDavidSantiago <david.ios.developer@gmail.com>
2026-06-11 10:50:08 -0500

    Feat: before migrating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { Diagnostic } from '@awesome-cordova-plugins/diagnostic/ngx';
-import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-import { Camera } from '@awesome-cordova-plugins/camera/ngx';
-import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx';
-import { Clipboard } from '@awesome-cordova-plugins/clipboard/ngx';
-import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
-import { AppVersion } from '@awesome-cordova-plugins/app-version/ngx';
-import { OpenNativeSettings } from '@awesome-cordova-plugins/open-native-settings/ngx';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    Diagnostic,
-    Geolocation,
-    Camera,
-    Clipboard,
-    StatusBar,
-    AppVersion,
-    OpenNativeSettings,
-    LocationAccuracy
   ],
   bootstrap: [AppComponent],
 })
